4a925aa13506c8a1b9fbb2c07103f5641906cbd7
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-02-11 15:40:43 +0000

    feat: added "completed" display to TaskItem

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react"
 import { Task } from "../../../types/Task"
+import { TaskContext } from "../../../context/TaskContext"
 
 type TaskItemProps = {
   itemData: Task
 }
 
 export default function TaskItem({ itemData }: TaskItemProps) {
+  const { toggleTaskComplete } = useContext(TaskContext)
+
   const priorityClass =
     itemData.priority === "low"
       ? "bg-slate-500 text-white"
@@ -16,6 +20,9 @@ export default function TaskItem({ itemData }: TaskItemProps) {
 
   return (
     <div className="border-1 m-2 flex flex-col justify-center items-center gap-1 p-2">
+      <p className="cursor-pointer" onClick={() => toggleTaskComplete(itemData.id)}>
+        completed: {itemData.completed.toString()}
+      </p>
       <p>{itemData.createdAt.toLocaleString()}</p>
       <h2>{itemData.title}</h2>
       <p>{itemData.description}</p>
