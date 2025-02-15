346ef04c31b2c23d582636058ac15caed09ddeab
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-02-15 00:40:00 +0000

    tests: wrote tests for deleteTask and editTask

--- context/TaskContext.test.tsx
+++ context/TaskContext.test.tsx
@@ -4,7 +4,7 @@ import { render, screen, waitFor, fireEvent } from "@testing-library/react"
 import { describe, it, expect } from "vitest"
 
 function TestComponent() {
-  const { tasks, createTask } = useContext(TaskContext)
+  const { tasks, createTask, deleteTask, editTask } = useContext(TaskContext)
 
   return (
     <>
@@ -12,6 +12,22 @@ function TestComponent() {
       <button data-testid="create-btn" onClick={() => createTask({ title: "New Task", priority: "low" })}>
         Add Task
       </button>
+      <button data-testid="delete-btn" onClick={() => tasks[0] && deleteTask(tasks[0].id)}>
+        Delete Task
+      </button>
+
+      <button
+        data-testid="edit-btn"
+        onClick={() => tasks[0] && editTask(tasks[0].id, { status: "pending", title: "Edited Task", priority: "low" })}
+      >
+        Edit Task
+      </button>
+
+      {tasks[0] && (
+        <div data-testid="task-div">
+          <p data-testid="task-div-title">{tasks[0].title}</p>
+        </div>
+      )}
     </>
   )
 }
@@ -39,6 +55,54 @@ describe("TaskContext", () => {
 
     await waitFor(() => {
       expect(screen.getByTestId("tasks-count")).toHaveTextContent("Tasks: 1")
+    })
+  })
+
+  it("deletes a task when deleteTask is called", async () => {
+    render(
+      <TaskContextProvider>
+        <TestComponent />
+      </TaskContextProvider>
+    )
+
+    const button = screen.getByTestId("create-btn")
+    fireEvent.click(button)
+
+    await waitFor(() => {
+      expect(screen.getByTestId("tasks-count")).toHaveTextContent("Tasks: 1")
+      expect(screen.getByTestId("task-div")).toBeInTheDocument()
+    })
+
+    const deleteBtn = screen.getByTestId("delete-btn")
+    fireEvent.click(deleteBtn)
+
+    await waitFor(() => {
+      expect(screen.getByTestId("tasks-count")).toHaveTextContent("Tasks: 0")
+      expect(screen.queryByTestId("task-div")).not.toBeInTheDocument()
+    })
+  })
+
+  it("edits a task when editTask is called", async () => {
+    render(
+      <TaskContextProvider>
+        <TestComponent />
+      </TaskContextProvider>
+    )
+
+    const button = screen.getByTestId("create-btn")
+    fireEvent.click(button)
+
+    await waitFor(() => {
+      expect(screen.getByTestId("tasks-count")).toHaveTextContent("Tasks: 1")
+      expect(screen.getByTestId("task-div")).toBeInTheDocument()
+      expect(screen.getByTestId("task-div-title")).toHaveTextContent("New Task")
+    })
+
+    const editBtn = screen.getByTestId("edit-btn")
+    fireEvent.click(editBtn)
+
+    await waitFor(() => {
+      expect(screen.getByTestId("task-div-title")).toHaveTextContent("Edited Task")
     })
   })
 })
