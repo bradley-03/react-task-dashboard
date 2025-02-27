d9153aaccbd3a6b8bcb8092f6df1b5f2ac038fea
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-02-27 16:36:30 +0000

    feat: added display toggle between list mode and kanban board

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import TaskForm from "./components/TaskForm/TaskForm"
 import TaskList from "./components/TaskList/TaskList"
 import useDarkMode from "../hooks/useDarkMode"
 import KanbanBoard from "./components/KanbanBoard/KanbanBoard"
+import { useState } from "react"
 
 function App() {
   const { darkMode, setDarkMode } = useDarkMode()
+  const [currentDisplay, setCurrentDisplay] = useState<"kanban" | "list">("list")
+
+  function toggleDisplay() {
+    setCurrentDisplay(prev => (prev === "kanban" ? "list" : "kanban"))
+  }
 
   return (
     <TaskContextProvider>
+      <button onClick={toggleDisplay}>{currentDisplay}</button>
+      <br></br>
       <button onClick={() => setDarkMode(prev => !prev)}>Turn Dark Mode {darkMode ? "Off" : "On"}</button>
       <TaskForm />
-      {/* <TaskList /> */}
-      <KanbanBoard />
+
+      {currentDisplay === "kanban" ? <KanbanBoard /> : <TaskList />}
     </TaskContextProvider>
   )
 }
