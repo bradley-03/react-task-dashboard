0b78b21a96c1cd1a7ab50c24c7f24f7f7b9566cf
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-02-27 12:06:27 +0000

    (wip): kanban board starter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { TaskContextProvider } from "../context/TaskContext"
 import TaskForm from "./components/TaskForm/TaskForm"
 import TaskList from "./components/TaskList/TaskList"
 import useDarkMode from "../hooks/useDarkMode"
+import KanbanBoard from "./components/KanbanBoard/KanbanBoard"
 
 function App() {
   const { darkMode, setDarkMode } = useDarkMode()
@@ -11,6 +12,7 @@ function App() {
       <button onClick={() => setDarkMode(prev => !prev)}>Turn Dark Mode {darkMode ? "Off" : "On"}</button>
       <TaskForm />
       <TaskList />
+      <KanbanBoard />
     </TaskContextProvider>
   )
 }
