6c0ec0233d3ded6304dcb3993f80cd4b619ad874
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-03-20 12:39:20 +0000

    styles: added appropriate styles to Select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,15 +1,3 @@
-// export default function Select({ children, ...props }: React.ComponentProps<"select">) {
-//   const selectStyles = `dark:bg-black dark:text-white bg-white text-black dark:disabled:bg-neutral-700 disabled:bg-neutral-200 disabled:cursor-not-allowed placeholder:text-neutral-400 w-full min-w-0 rounded-md transition px-3 py-2 dark:outline-white focus:outline-1 outline-neutral-600 dark:border-neutral-600 border-neutral-300 border-1 shadow ${
-//     props.className || ""
-//   }`
-
-//   return (
-//     <select {...props} className={selectStyles}>
-//       {children}
-//     </select>
-//   )
-// }
-
 import { default as RSelect } from "react-select"
 import { GroupBase, Props } from "react-select"
 
@@ -23,11 +11,20 @@ export default function Select<
       {...props}
       unstyled
       classNames={{
-        container: state => `rounded-md transition shadow ${state.isFocused && "outline-1 outline-neutral-600"}`,
-        control: () => `bg-white h-9 text-black text-left rounded-md border-1 border-neutral-300 px-3 py-2`,
-        menu: () => `mt-2 rounded-md overflow-hidden border-1 border-neutral-300`,
+        container: state =>
+          `rounded-md transition shadow ${state.isFocused && " outline-1 dark:outline-white outline-neutral-600"}`,
+        control: state =>
+          ` h-9 text-left rounded-md border-1 border-neutral-300 dark:border-neutral-600 px-3 py-2 ${
+            state.isDisabled
+              ? "dark:bg-neutral-700 bg-neutral-200 text-neutral-500 dark:text-neutral-300"
+              : "bg-white dark:bg-black dark:text-white text-black"
+          }`,
+        menu: () =>
+          `mt-2 dark:bg-black bg-white rounded-md overflow-hidden border-1 dark:border-neutral-600 border-neutral-300 shadow p-1`,
         option: () =>
-          `bg-white px-2 py-1 hover:cursor-pointer hover:bg-neutral-200 transition text-left border-neutral-300 border-1 border-collapse`,
+          `bg-white dark:bg-black px-2 rounded-md py-1 dark:hover:bg-neutral-600 hover:bg-neutral-200 text-left`,
+        valueContainer: state => (state.isDisabled ? "cursor-not-allowed" : "cursor-pointer"),
+        dropdownIndicator: state => (state.isDisabled ? "cursor-not-allowed" : "cursor-pointer"),
       }}
     />
   )
