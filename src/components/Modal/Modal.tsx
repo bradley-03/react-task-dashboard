af01706cc2ebc3440effa2ff6e51cd853df873d5
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-03-27 14:11:37 +0000

    style: fixed modal sizing when screen Y size is too small

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -19,9 +19,12 @@ export default function Modal({ isOpen, onClose, title, children }: ModalProps) 
   }
 
   return createPortal(
-    <div className="fixed inset-0 flex items-center justify-center bg-black/50" onMouseDown={handleMouseDown}>
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black/50 overflow-y-auto"
+      onMouseDown={handleMouseDown}
+    >
       <div
-        className="dark:bg-black dark:border-1 dark:border-neutral-600 dark:text-white bg-white text-black p-6 rounded-lg shadow-lg w-96"
+        className="dark:bg-black dark:border-1 dark:border-neutral-600 dark:text-white bg-white text-black p-6 rounded-lg shadow-lg w-96 overflow-y-auto max-h-[90vh]"
         // onMouseDown={e => e.stopPropagation()}
       >
         <div className="flex justify-between items-center mb-4">
