767f4a5c27f413681ed21379e3c96234ace6efa7
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-03-28 14:19:49 +0000

    styles: made delete modal look nicer

--- src/components/Modals/DeleteTaskModal/DeleteTaskModal.tsx
+++ src/components/Modals/DeleteTaskModal/DeleteTaskModal.tsx
@@ -1,6 +1,7 @@
 import Modal from "../../Modal/Modal"
 import { Task } from "../../../../types/Task"
 import Button from "../../Button/Button"
+import Divider from "../../Divider/Divider"
 
 type DeleteTaskModalProps = {
   isOpen: boolean
@@ -12,12 +13,20 @@ type DeleteTaskModalProps = {
 export default function DeleteTaskModal({ isOpen, onClose, onDelete, task }: DeleteTaskModalProps) {
   return (
     <Modal title="Edit Task" isOpen={isOpen} onClose={onClose}>
-      <p>
-        Are you sure you want to delete the task <span className="font-bold">{task.title}?</span>
-      </p>
-      <Button variant="danger" onClick={() => task && onDelete(task.id)}>
-        Delete
-      </Button>
+      <div className="flex flex-col">
+        <p>
+          Are you sure you want to delete the task <span className="font-bold">{task.title}?</span>
+        </p>
+        <Divider />
+        <div className="self-end flex gap-4">
+          <Button onClick={onClose} variant="secondary">
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={() => task && onDelete(task.id)}>
+            Delete
+          </Button>
+        </div>
+      </div>
     </Modal>
   )
 }
